555bd9fb22676c54f9adeb5b05ceda6fe692bb3f
Sahil <sahildudhat1919@gmail.com>
2024-04-12 10:41:05 +0530

    12-04-2024

---
+++ Lecture-17/newapp/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import BasicTable from './Component/Table'
+
+function App() {
+  return (
+    <div className="App">
+      <BasicTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture-17/newapp/src/Component/Table.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+export default function BasicTable() {
+
+    const [rows, setrows] = React.useState([]);
+
+    React.useState(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((Data)=>{
+            setrows(Data)
+        })        
+    },[])
+    console.log(rows);
+  return (
+    <>
+      <h1>Table Demo Using Material UI</h1>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="center">User Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">City</TableCell>
+            <TableCell align="center">Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.address.city}</TableCell>
+              <TableCell align="center">{row.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+    
+  );
+}
